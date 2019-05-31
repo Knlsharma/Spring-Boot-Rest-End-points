4e63ea18769fd7b08e0002b25e330727c9d20aee
kunal sharma <knlsharma678@gmail.com>
2019-05-31 21:51:55 +0530

    code added

---
+++ src/main/java/com/mycompany/spring_boot_starter/first.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spring_boot_starter;
+
+/**
+ *
+ * @author knl
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class first {
+  public static void main(String args[])
+    {
+
+      SpringApplication.run(first.class , args);
+
+    }
+
+}
